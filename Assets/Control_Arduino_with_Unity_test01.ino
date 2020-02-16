7b971be76aa21daab89690e695da20f5dd4f5170
Roselynn Conrady <conrady.roselynn@gmail.com>
2020-02-15 17:13:41 -0800

    added arduino sketch

---
+++ Assets/Control_Arduino_with_Unity_test01.ino
@@ -0,0 +1,24 @@
+// This belongs with the Unity Project called
+// "Contro_Arduino_with_Unity_01"
+
+int data;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available()) {
+    data = Serial.read();
+    if (data == 'A') {
+      digitalWrite(13,HIGH);
+    }
+    else {
+      digitalWrite(13,LOW);
+      
+    }
+  }
+}
